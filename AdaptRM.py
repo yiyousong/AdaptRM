4ba3e4b962fb4c62d2c264fc940785b0ae03a08b
Y Song <56291756+yiyousong@users.noreply.github.com>
2022-09-05 17:17:00 +0800

    Add files via upload

---
+++ AdaptRM.py
@@ -0,0 +1,90 @@
+import numpy as np
+import torch
+import sys
+from sklearn import metrics
+import torch.nn as nn
+import torch.nn.functional as F
+import math, copy, time
+import scripts as scrip
+import argparse
+header='/home/yiyou/model'
+from sublayers import *
+torch.set_num_threads(1)
+np.random.seed(7)
+parser = argparse.ArgumentParser()
+parser.add_argument('--model', default=3,help='Weak,Adapt,Encoder,Mixer')
+parser.add_argument('--embedsize', default=1,help='4^__size__')
+parser.add_argument('--drop', default=0.2,help='dropout')
+parser.add_argument('--act', default='relu')
+parser.add_argument('--out',default=None,help='feedback output')
+
+args, unknown = parser.parse_known_args()
+num_epoch=20
+src_vocab=4**int(args.embedsize)
+model_list=['Weak','Adapt','Encoder','Mixer']
+drop=float(args.drop)
+
+if args.out is None:
+    args.out='%s/Multi_%s_embedsize%d_dropout%.2f_dim%d_depth%d_act%s_sep_%s.out'%(header,model_list[int(args.model)],int(args.embedsize),drop,dim,depth,args.act,args.sep)
+def loaddata(i):
+    data, label = np2tensor(np.load("/data/yiyou/Multi_WeakRM/multi_shuffled/data_%d" % (i), allow_pickle=True),
+                                np.load("/data/yiyou/Multi_WeakRM/multi_shuffled/label_%d" % (i), allow_pickle=True)
+                            ,mer=int(args.embedsize))
+
+    return data, label
+
+if __name__ == '__main__':
+    sys.stdout = open(args.out, 'w')
+    if int(args.model) == 0:
+        model = nn.Sequential(ReshapeTrim(),
+                              Conv1dtranspose(in_chan=src_vocab, kernel_size=7, in_transpose=True,dropout=drop,acti=args.act, out_chan=64,pooling=True),
+                              Conv1dtranspose(in_chan=64, kernel_size=5, out_chan=16,pooling=True,dropout=drop,acti=args.act),
+                              AttentionMerge(out_size=100, in_size=6 * 16,acti=args.act))
+    elif int(args.model) == 2:
+        model=Transformer(src_vocab=src_vocab,d_model=256,dropout=drop,outsize=100,acti=args.act)
+    elif int(args.model) == 3:
+        kernel_size = int(args.kernelsize)
+        adaptout = int(args.adaptout)
+        print('kernel_size=%d,adaptout=%d', (kernel_size, adaptout), flush=True)
+        model = ConvMixer(vocab=src_vocab, sephead=args.sep, n_classes=100, dim=256, depth=10, dropout=drop,
+                          kernel_size=kernel_size, adaptout=adaptout, ac1=args.act, ac2=args.act)
+    else:
+        model = nn.Sequential(Conv1dtranspose(in_chan=src_vocab, kernel_size=7, in_transpose=True, out_chan=32),
+                              Conv1dtranspose(in_chan=32, kernel_size=7, out_chan=64, dropout=drop, acti=args.act),
+                              nn.AdaptiveAvgPool1d(15),
+                              Conv1dtranspose(in_chan=64, kernel_size=7, out_chan=32, dropout=drop, acti=args.act),
+                              Conv1dtranspose(in_chan=32, kernel_size=7, out_chan=64, dropout=drop, acti=args.act),
+                              Linout(in_size=3 * 64, acti=args.act, out_size=100)
+                              )
+        model = model.cuda()
+    for p in model.parameters():
+        if p.dim() > 1:
+            nn.init.xavier_uniform_(p)
+    optimizer = torch.optim.Adam(
+        model.parameters(), lr=5e-5)
+    loss_func = torch.nn.functional.binary_cross_entropy
+    tmp=np.arange(5)
+    for k in range(num_epoch):
+        for j in tmp:
+            data_list=[]
+            label_list=[]
+            for i in range(4):
+                data,label=loaddata(i+4*j)
+                data_list.extend(data)
+                label_list.extend(label)
+            test_data,test_label=loaddata(20+j)
+            scrip.run_epoch(model,data_list,label_list,test_data,test_label,optimizer=optimizer,loss_func=loss_func)
+        tmp=np.flip(tmp)
+    model_name='%s/Multi_%s_embedsize%d_dropout%.2f_dim%d_depth%d_act%s_sep_%s.model'%(header,model_list[int(args.model)],int(args.embedsize),drop,dim,depth,args.act,args.sep)
+    torch.save(model,model_name )
+    data_list = []
+    label_list = []
+    for i in range(5):
+        data,label = loaddata(20 + i)
+        data_list.extend(data)
+        label_list.extend(label)
+    print('total epoch%d'%(num_epoch))
+    print('FINAL AUROC')
+    auc=scrip.run_test_epoch(model, data_list, label_list)
+    print('average auc: %.4f'%(np.average(auc[:,0])))
+    sys.stdout.close()
---
+++ scripts.py
@@ -0,0 +1,222 @@
+import torch
+import numpy as np
+import pandas as pd
+from torch import nn
+from torch.utils.data import TensorDataset,DataLoader
+from sklearn import metrics
+
+out_list = np.array(['Adrenal'
+                        , 'Brainstem'
+                        , 'Cerebellum'
+                        , 'Cerebrum'
+                        , 'Colon'
+                        , 'EndoC'
+                        , 'endometrial'
+                        , 'Heart'
+                        , 'HSC'
+                        , 'Hypothalamus'
+                        , 'islet'
+                        , 'Kidney'
+                        , 'Liver'
+                        , 'lung'
+                        , 'lymphocyte'
+                        , 'Muscle'
+                        , 'ovary'
+                        , 'Prostate'
+                        , 'Rectum'
+                        , 'RWPE'
+                        , 'Skin'
+                        , 'Stomach'
+                        , 'Testis'
+                        , 'Thyroid'
+                        , 'Urinary'
+                        , 'ac4c'
+                        , 'hm5c'
+                        , 'm7g'
+                        , 'h_b'
+                        , 'h_k'
+                        , 'h_l'
+                        , 'm_b'
+                        , 'm_h'
+                        , 'm_k'
+                        , 'm_l'
+                        , 'm_t'
+                        , 'r_b'
+                        , 'r_k'
+                        , 'r_l'
+                        ,'m1A_Ctrl'
+                        ,'m5C_Ctrl'
+                        ,'m6A_Ctrl'
+                        ,'m7G_Ctrl'
+                        ,'m1A_Hypo'
+                        ,'m5C_Hypo'
+                        ,'m6A_Hypo'
+                        ,'m7G_Hypo'  ])
+max_nclass=100
+
+def run_train_epoch(model,data,labelsign,optimizer,loss_func,pos_weight=1):
+    pred_list=[]
+    for i in range(len(data)):
+        data1=data[i]
+        label1=labelsign[i]
+        pred=model(data1)
+        optimizer.zero_grad()
+        loss=signloss(loss_func,pred,label1,pos_weight=pos_weight)
+        loss.backward()
+        optimizer.step()
+        pred_list.append(pred)
+    result=evaluate(pred_list,labelsign,prefix='train')
+    return result
+def signloss(loss_func,pred,labelsign,mask_by_weight=True,pos_weight=1):
+        mask=labelsign**2
+        mask[labelsign>0]=pos_weight
+        label=(labelsign+1)//2
+        if mask_by_weight:
+            loss=loss_func(pred,label,weight=mask)
+        else:
+            label=label[mask]
+            pred=pred[mask]
+            loss=loss_func(pred,label)
+        return loss
+def evaluate(pred_list,labelsign=None,prefix=''):
+    pred_list = torch.cat(pred_list).cpu().detach().numpy()
+    label = torch.cat(labelsign).cpu().numpy()
+    labelexist = np.sum(label ** 2, axis=0)
+    index = np.arange(labelexist.shape[0])
+    index = index[labelexist != 0]
+    result=[]
+    for i in index:
+        y = label[:, i]
+        pred = pred_list[:, i]
+        pred = pred[y != 0]
+        predint=(pred+0.5).astype(int)
+        y = (y[y != 0] + 1) // 2
+        y=y.astype(int)
+        p=y[pred>0.5]
+        n=y[pred<0.5]
+        tp=np.sum(p)
+        fn=np.sum(1-p)
+        tn=np.sum(1-n)
+        fp=np.sum(n)
+        sense=tp/(fn+tp)
+        speci=tn/(tn+fp)
+        auc = metrics.roc_auc_score(y, pred)
+        if np.sum(predint)*np.sum(1-predint)!=0:
+            f1=metrics.f1_score(y,predint)
+            mcc=metrics.matthews_corrcoef(y,predint)
+        else:
+            f1=mcc=0
+        pr=metrics.average_precision_score(y,pred)
+        result.append(np.array([auc,f1,mcc,pr,sense,speci]))
+        # result.append(auc)
+        print('%stask %d:\tauc: %.3f\tf1: %.3f\tmcc: %.3f\tap: %.3f\tsenseticity: %.3f\tspecificity: %.3f\t'% (prefix,i, auc,f1,mcc,pr,sense,speci), flush=True)
+    result=np.array(result)
+    return result
+@torch.no_grad()
+def run_test_epoch(model,data,labelsign):
+    model.eval()
+    pred_list = []
+    for i in range(len(data)):
+        data1= data[i]
+        pred=model(data1)
+        pred_list.append(pred)
+    result=evaluate(pred_list,labelsign,prefix='test ')
+    model.train()
+    return result
+def np2tensor(input_list, label_list=None, mer=1,newvar=0,minlength=0,maxlength=5000, chunk=False,instance_length=40,shuffle=False,train_ratio=0.8,posrep=1,negrep=1):
+    input_tensor_list = []
+    if label_list is not None:
+        label_tensor_list = []
+    index = np.arange(len(input_list))
+    if shuffle:
+        np.random.shuffle(index)
+    for i in index:
+        if label_list is not None:
+            label = np.reshape(np.array(label_list[i]),[1,-1])
+        input = np.squeeze(np.int32(input_list[i]))
+        input = np.reshape(input, [-1, 4])
+        if input.shape[0]>minlength:
+            if input.shape[0]<maxlength:
+                index1 = np.where(input == 1)[-1]
+                if mer > 1:
+                    idx = index1[mer - 1:]
+                    for k in range(mer - 1):
+                        idx = idx + (4 ** (mer - 1 - k)) * index1[k:k - mer + 1]
+                    data = np.zeros([input.shape[0] - mer + 1, 4 ** mer + newvar])
+                    data[np.where(input == 1)[0][:-mer + 1], idx] = 1
+                else:
+                    data=input
+                if chunk:
+                    data = data[:(len(data) // (instance_length - mer + 1)) * (instance_length - mer + 1)]
+                    data = np.reshape(data, [-1, instance_length - mer + 1, 4 ** mer + newvar])
+                else:
+                    if mer >1:
+                        data = np.reshape(data, [1, -1, 4 ** mer + newvar])
+                    else:
+                        data = np.reshape(input, [1, -1, 4])
+                        if newvar > 0:
+                            data = np.append(data, np.zeros([1, data.shape[1], newvar]), axis=-1)
+                input = torch.as_tensor(data).float().cuda()
+                input_tensor_list.append(input)
+                if label_list is not None:
+                    rep=0
+                    if negrep > 1:
+                        if np.sum(label) < 0:
+                            rep = negrep
+                    if posrep > 1:
+                        if np.sum(label) > 0:
+                            rep = posrep
+
+                    label = torch.as_tensor(label).float().cuda()
+                    label_tensor_list.append(label)
+                    if rep>0:
+                        for i in range(rep):
+                            label_tensor_list.append(label)
+                            input_tensor_list.append(input)
+
+    if label_list is not None:
+        return input_tensor_list, label_tensor_list
+    else:
+        return input_tensor_list
+
+
+def run_epoch(model,traindata,trainlabelsign,testdata,testlabelsign,optimizer,loss_func,pos_weight=1):
+    train_auc=run_train_epoch(model, traindata, trainlabelsign, optimizer, loss_func,pos_weight=pos_weight)
+    test_auc=run_test_epoch(model, testdata,testlabelsign)
+    return train_auc,test_auc
+def loadsingledata(query,mer=1,newvar=0,expand_label=True,shuffle=False,minlength=0,maxlength=1000,posrep=1,negrep=1,train_ratio=0.8):
+    if type(query) is str:
+        if not query.isnumeric():
+            query = np.where(out_list == query)[0][0]
+        else:
+            query = int(query)
+    if query<28:
+        data=np.load('/data/yiyou/Multi_WeakRM/%s_positive.pickle'%(out_list[query]),allow_pickle=True)
+        neg_data=np.load('/data/yiyou/Multi_WeakRM/%s_negative.pickle'%(out_list[query]),allow_pickle=True)
+        label=np.append(np.ones(len(data)),np.ones(len(neg_data))*-1)
+        multi_label=np.zeros([len(label),max_nclass])
+        multi_label[:,query]=label
+        data.extend(neg_data)
+        #sample using random.sample(dataset,n)
+    elif query < 39:
+        data = np.load('/data/yiyou/independent/sin%d.npy'%(query-28))
+        label = np.load('/data/yiyou/independent/sin%d.label.npy'%(query-28))[:,query-28]
+        multi_label = np.zeros([len(label), max_nclass])
+        multi_label[:, query] = label
+        # sample using random.sample(dataset,n)
+
+    else:
+        data = np.load('/data/yiyou/Multi_WeakRM/zebrafish/%s_positive.pickle' % (out_list[query]), allow_pickle=True)
+        neg_data = np.load('/data/yiyou/Multi_WeakRM/zebrafish/%s_negative.pickle' % (out_list[query]), allow_pickle=True)
+        label = np.append(np.ones(len(data)), np.ones(len(neg_data)) * -1)
+        multi_label = np.zeros([len(label), max_nclass])
+        multi_label[:, query] = label
+        data.extend(neg_data)
+    if not expand_label:
+        multi_label=label
+    datatensor,labeltensor=np2tensor(data,multi_label,mer=mer,newvar=newvar,shuffle=shuffle,minlength=minlength,maxlength=maxlength,posrep=posrep,negrep=negrep,train_ratio=train_ratio)
+    return datatensor,labeltensor
+
+
+
+
